6b270b50e9091cb741eccdc43987169be2d9fc4f
Deepti Singh <iamdeepti956@gmail.com>
2020-05-09 19:19:38 +0530

    update Home

--- client/src/component/pages/Home.js
+++ client/src/component/pages/Home.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import {Image} from 'react-bootstrap';
-import ShECURE from '../images/ShECURE.png';
+import shecure from '../images/shecure.png';
 import "../../App.css";
 export class Home extends Component {
     render() {
         return (
             <div className='container'>
                 
-                <Image src={ShECURE} className='img-home'/>
+                <Image src={shecure} className='img-home'/>
             </div>
         )
     }
